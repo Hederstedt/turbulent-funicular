239d43f56c3f9148a23f663069f727700732928b
Mike <mhederstedt@me.com>
2015-12-03 10:33:40 +0100

    First commit

---
+++ testFörGitHub/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testFörGitHub
+{
+    class Class1
+    {
+    }
+}
